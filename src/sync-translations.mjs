080b009b927a15263cbd1c1269892ef9dea0e240
flyx86 <259150985+flyx86@users.noreply.github.com>
2026-03-29 14:39:57 +0200

    keep newline at end of xml files

--- src/sync-translations.mjs
+++ src/sync-translations.mjs
@@ -3,6 +3,7 @@ import fs from "fs";
 
 const DEFAULT_LANG = "en";
 const LANG_DIR = "./langs";
+const EOL = "\n";
 
 const parser = new XMLParser({
   ignoreAttributes: false,
@@ -21,7 +22,7 @@ function readXml(lang) {
 }
 
 function writeXml(lang, xml) {
-  fs.writeFileSync(`${LANG_DIR}/Cafe_${lang}.xml`, xml, "utf-8");
+  fs.writeFileSync(`${LANG_DIR}/Cafe_${lang}.xml`, xml.concat(EOL), "utf-8");
 }
 
 function collapseEmptyTags(contents) {
